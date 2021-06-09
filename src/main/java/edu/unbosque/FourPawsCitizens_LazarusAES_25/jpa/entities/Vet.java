5de4b3d7b09e10e2e9e076317ccccc74cf5511cc
Esteban <lecardenasc@unbosque.edu.co>
2021-06-09 17:12:08 -0500

    visit and vet relation

--- src/main/java/edu/unbosque/FourPawsCitizens_LazarusAES_25/jpa/entities/Vet.java
+++ src/main/java/edu/unbosque/FourPawsCitizens_LazarusAES_25/jpa/entities/Vet.java
@@ -1,14 +1,17 @@
 package edu.unbosque.FourPawsCitizens_LazarusAES_25.jpa.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "Vet")
 @PrimaryKeyJoinColumn
 public class Vet extends UserApp{
+
+    @GeneratedValue
+    @Column(name = "vet_id", nullable = false)
+    private Integer vet_id;
 
     @Column(name = "name", nullable = false)
     private String name;
@@ -19,15 +22,27 @@ public class Vet extends UserApp{
     @Column(name = "neighborhood", nullable = false)
     private String neighborhood;
 
+    @OneToMany(mappedBy = "vet", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Visit> visits = new ArrayList<>();
+
     public Vet(){
 
     }
 
-    public Vet(String username, String password, String email, String name, String address, String neighborhood) {
-        super(username, password, email, "vet");
+    public Vet(String username, String password, String email,  Integer vet_id, String name, String address, String neighborhood) {
+        super(username, password, email, "Vet");
+        this.vet_id = vet_id;
         this.name = name;
         this.address = address;
         this.neighborhood = neighborhood;
+    }
+
+    public Integer getVet_id() {
+        return vet_id;
+    }
+
+    public void setVet_id(Integer vet_id) {
+        this.vet_id = vet_id;
     }
 
     public String getName() {
@@ -52,5 +67,14 @@ public class Vet extends UserApp{
 
     public void setNeighborhood(String neighborhood) {
         this.neighborhood = neighborhood;
+    }
+
+    public List<Visit> getVisits() {
+        return visits;
+    }
+
+    public void addVisit(Visit visit) {
+        visits.add(visit);
+        visit.setVet(this);
     }
 }
---
+++ src/main/java/edu/unbosque/FourPawsCitizens_LazarusAES_25/jpa/entities/Visit.java
@@ -0,0 +1,77 @@
+package edu.unbosque.FourPawsCitizens_LazarusAES_25.jpa.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Visit")
+public class Visit {
+
+    @Id
+    @Column(name = "visit_id")
+    private Integer visit;
+
+    @Column(name = "created_id")
+    private String created_id;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "vet_id")
+    private Vet vet;
+
+    public Visit(){
+
+    }
+
+    public Visit(Integer visit, String created_id, String type, String description, Vet vet) {
+        this.visit = visit;
+        this.created_id = created_id;
+        this.type = type;
+        this.description = description;
+        this.vet = vet;
+    }
+
+    public Integer getVisit() {
+        return visit;
+    }
+
+    public void setVisit(Integer visit) {
+        this.visit = visit;
+    }
+
+    public String getCreated_id() {
+        return created_id;
+    }
+
+    public void setCreated_id(String created_id) {
+        this.created_id = created_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Vet getVet() {
+        return vet;
+    }
+
+    public void setVet(Vet vet) {
+        this.vet = vet;
+    }
+}
