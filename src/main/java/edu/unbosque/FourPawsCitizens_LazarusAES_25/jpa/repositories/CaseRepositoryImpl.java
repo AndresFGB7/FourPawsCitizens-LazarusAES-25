e916130f7b02164d804836b120d02db6e3788054
cdsanchezm <cdsanchezm@unbosque.edu.co>
2021-06-16 21:06:49 -0500

    vet y pet

--- src/main/java/edu/unbosque/FourPawsCitizens_LazarusAES_25/jpa/repositories/CaseRepositoryImpl.java
+++ src/main/java/edu/unbosque/FourPawsCitizens_LazarusAES_25/jpa/repositories/CaseRepositoryImpl.java
@@ -2,8 +2,13 @@ package edu.unbosque.FourPawsCitizens_LazarusAES_25.jpa.repositories;
 
 import edu.unbosque.FourPawsCitizens_LazarusAES_25.jpa.entities.Case;
 
+import javax.persistence.EntityManager;
+
 public class CaseRepositoryImpl {
     Case c;
+
+
+
     void CaseRepositoryImpl(){
 
     }
